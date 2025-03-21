2fc2662b80865b45c72450c24dfb7fc48a94cfff
J <hola@javiergarcia.com>
2025-03-21 01:24:33 +0100

    prueba

--- backend/settings.py
+++ backend/settings.py
@@ -129,5 +129,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ['localhost', '127.0.0.1', '.vercel.app']
+# CORS_ALLOWED_ORIGINS = ['localhost', '127.0.0.1', '.vercel.app']
