f055edc19dcbe73440a9c4d2b2bc16c3b162e3ed
J <hola@javiergarcia.com>
2025-03-31 18:43:25 +0200

    casa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server: {
-    https: {
-      key: fs.readFileSync('C:/Users/J/Documents/ppprueba/key.pem'),  // Ruta de la clave privada
-      cert: fs.readFileSync('C:/Users/J/Documents/ppprueba/cert.pem'), // Ruta del certificado
-    }
-  }
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('C:/Users/J/Documents/ppprueba/key.pem'),  // Ruta de la clave privada
+  //     cert: fs.readFileSync('C:/Users/J/Documents/ppprueba/cert.pem'), // Ruta del certificado
+  //   }
+  // }
 })
